65d99dcabbcf9521654999f0b1472be8a17ad8fd
Ming <harry21105@gmail.com>
2021-10-22 22:18:47 +0800

    update

---
+++ tribfibonacci.py
@@ -0,0 +1,14 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        trib=[]
+        trib.append(0)
+        trib.append(1)
+        trib.append(1)
+        
+        if n<3:
+            return trib[n]
+        
+        for i in range(3,n+1):
+            sum=trib[i-3]+trib[i-2]+trib[i-1]
+            trib.append(sum)
+        return trib[n]
